e860a89cc1288c57de13b08a46f4de97c9fd1113
miguel9395 <90016689+miguel9395@users.noreply.github.com>
2022-09-15 10:20:14 -0500

    Add files via upload

---
+++ Ejercicio 4.py
@@ -0,0 +1,10 @@
+########Ejercicio 4 Miguel Zabala############
+
+contadorinverso= 100
+
+while contadorinverso >= 1:
+    print('Contador Vale:', contadorinverso)
+    contadorinverso -= 1
+print('Fin del Contador Inverso')
+
+##########Fin Ejercicio######################
